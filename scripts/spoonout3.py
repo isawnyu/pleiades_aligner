202743a779502b6209b31fac8305ae019e00420b
Tom Elliott <ipse@paregorios.org>
2024-02-09 06:03:31 -0600

    add spoonout script variant that filters by included namespaces

---
+++ scripts/spoonout3.py
@@ -0,0 +1,154 @@
+#
+# This file is part of pleiades_aligner
+# by Tom Elliott for the Institute for the Study of the Ancient World
+# (c) Copyright 2023 by New York University
+# Licensed under the AGPL-3.0; see LICENSE.txt file.
+#
+"""
+change me
+"""
+
+from airtight.cli import configure_commandline
+from colorama import Fore, Back, Style
+import json
+import logging
+import math
+from pathlib import Path
+from slugify import slugify
+
+logger = logging.getLogger(__name__)
+
+DEFAULT_LOG_LEVEL = logging.WARNING
+OPTIONAL_ARGUMENTS = [
+    [
+        "-l",
+        "--loglevel",
+        "NOTSET",
+        "desired logging level ("
+        + "case-insensitive string: DEBUG, INFO, WARNING, or ERROR",
+        False,
+    ],
+    ["-v", "--verbose", False, "verbose output (logging level == INFO)", False],
+    [
+        "-w",
+        "--veryverbose",
+        False,
+        "very verbose output (logging level == DEBUG)",
+        False,
+    ],
+    [
+        "-i",
+        "--include",
+        "",
+        "filter results to require inclusion of these namespaces",
+        False,
+    ],
+    ["-o", "--operator", "AND", "operator for inclusion namespaces (AND or OR)", False],
+]
+POSITIONAL_ARGUMENTS = [
+    # each row is a list with 3 elements: name, type, help
+    [
+        "aligned_places_file",
+        str,
+        "path to aligned places json file as output by prioritize.py",
+    ]
+]
+
+
+def wipe_terminal():
+    print("\033[H\033[2J", end="")
+
+
+def output_place(pid: str, place: dict):
+    namespace = pid.split(":")[0]
+    if namespace == "pleiades":
+        color = Fore.BLUE
+    else:
+        color = Fore.RESET
+    try:
+        print(
+            color
+            + Style.BRIGHT
+            + f"{pid}:\n{place['title']}"
+            + Style.NORMAL
+            + Fore.RESET
+        )
+    except KeyError:
+        print(color + Style.BRIGHT + pid + ":" + Style.NORMAL + Fore.RESET)
+    try:
+        print(place["uri"])
+    except KeyError:
+        pass
+    try:
+        print(", ".join(sorted(place["names"], key=lambda n: slugify(n))))
+    except KeyError:
+        pass
+    try:
+        print(", ".join(sorted(place["feature_types"])))
+    except KeyError:
+        pass
+    try:
+        modes = place["modes"]
+    except KeyError:
+        modes = list()
+    else:
+        print(", ".join(sorted(modes)))
+    if "assertion" in modes or "inference" in modes:
+        ns = place["authorities"]
+        print(f"authorities: {', '.join(list(ns))}")
+    if "proximity" in modes:
+        d = place["centroid_distance_m"]
+        print(f"proximity:{place['proximity']}={math.ceil(d)}")
+    print()
+
+
+def main(**kwargs):
+    """
+    main function
+    """
+    # logger = logging.getLogger(sys._getframe().f_code.co_name)
+    whence = Path(kwargs["aligned_places_file"]).expanduser().resolve()
+    with open(whence, "r", encoding="utf-8") as f:
+        categorized_aligments = json.load(f)
+    del f
+
+    required_namespaces = set(v for v in kwargs["include"].split(",") if v)
+
+    for cat, aligned_places in categorized_aligments.items():
+        for pleiades_id, pleiades_data in aligned_places.items():
+            if required_namespaces:
+                these_namespaces = {pid.split(":")[0] for pid in pleiades_data.keys()}
+                logger.error(these_namespaces)
+                if kwargs["operator"] == "OR":
+                    if not required_namespaces.intersection(these_namespaces):
+                        continue
+                elif kwargs["operator"] == "AND":
+                    if not required_namespaces.issubset(these_namespaces):
+                        logger.error("skip")
+                        continue
+            wipe_terminal()
+            print(
+                Fore.RED
+                + Style.BRIGHT
+                + f"CATEGORY {cat}\n"
+                + "-" * 78
+                + Style.NORMAL
+                + Fore.RESET
+            )
+            output_place(pleiades_id, pleiades_data[pleiades_id])
+            for other_id, other_data in pleiades_data.items():
+                if other_id == pleiades_id:
+                    continue
+                output_place(other_id, other_data)
+            s = input("> ").strip()
+            if s:
+                if s[0].lower() == "q":
+                    exit()
+
+
+if __name__ == "__main__":
+    main(
+        **configure_commandline(
+            OPTIONAL_ARGUMENTS, POSITIONAL_ARGUMENTS, DEFAULT_LOG_LEVEL
+        )
+    )
